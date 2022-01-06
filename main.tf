5ae65233b948e9816d1407e5739e8f00918f0862
Ivan Kalashnikov <ivan@kalashnikov.uk>
2022-01-06 15:35:41 +0000

    added variable and extended gitignore

--- main.tf
+++ main.tf
@@ -12,6 +12,24 @@ resource "aws_instance" "app_instance" {
 	}
 	key_name = "eng99"
 }
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "eng99_ivan_vpc_terra"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.10.0/24", "10.0.20.0/24", "10.0.30.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
 
 # to run this use terraform plan
 # what plan does is a syntax check much like a --syntax-check
