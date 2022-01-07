bbc976a0c7aa76612d8112791588f6d3387b14f7
Ivan Kalashnikov <ivan@kalashnikov.uk>
2022-01-07 09:51:32 +0000

    added a readme

---
+++ README.md
@@ -0,0 +1,28 @@
+# Terraform
+
+## Installation
+
+**Arch Linux**
+`yay -S terraform`
+
+## Prerequisites 
+
+Adding two environment variables to the .bashrc
+
+`AWS_ACCESS_KEY_ID=<insert_key_here>`
+`AWS_SECRET_ACCESS_KEY=<insert_key_here>`
+`source ~/.bashrc`
+
+### Establish a working environment
+
+- Make a main.tf
+- variable.tf for all the variables
+
+### Useful commands
+
+`terraform init` for the initialisation
+`terraform plan` for a syntax check
+`terraform apply` to apply all the changes
+`terraform destroy` to destroy all changes made by terraform
+
+
--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_instance" {
-	ami = "ami-07d8796a2b0f8d29c"
+	ami = var.app_ami_id
 	instance_type = "t2.micro"
 	associate_public_ip_address = true
 	tags = {
@@ -15,15 +15,15 @@ resource "aws_instance" "app_instance" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "eng99_ivan_vpc_terra"
+  name = var.vpc_name 
   cidr = var.cidr_block
 
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   private_subnets = ["10.0.10.0/24", "10.0.20.0/24", "10.0.30.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+  enable_nat_gateway = false 
+  enable_vpn_gateway = false
 
   tags = {
     Terraform = "true"
@@ -34,3 +34,38 @@ module "vpc" {
 # to run this use terraform plan
 # what plan does is a syntax check much like a --syntax-check
 # terraform apply 
+resource "aws_security_group" "allow_tls" {
+  name        = "eng99_ivan_terraform"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "access the app from anywhere world"
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    description      = "ssh from world"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
